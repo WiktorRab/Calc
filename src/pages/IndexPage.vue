9dfb0de231f33af2bffd3e819dba1e290176693f
WiktorRab <wiktor.rabczuk@wp.pl>
2023-03-26 13:37:30 +0200

    v1.0

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,109 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <div class="row justify-center">
+      <div class="column justify-end q-gutter-y-sm" style="height: 90vh ">
+        <q-item class="bg-grey-3 q-mx-sm">
+          <q-item-section
+            class="text-h4 text-bold"
+            style="width: 85vw; max-width: 350px; height: 30vh; word-break: break-all; justify-content: unset;"
+          >
+            {{ wartosc }}
+          </q-item-section>
+        </q-item>
+        <div class="row justify-between q-mx-sm">
+          <CalcBtn @click=" wartosc=''" class="bg-blue-4" label="AC"/>
+          <CalcBtn @click="wartosc = wartosc + '*' + wartosc.slice(-1)" class="bg-blue-4" label="x²"/>
+          <CalcBtn @click="wartosc ='√'+ wartosc.slice(-1)" class="bg-blue-4" icon="√"/>
+          <CalcBtn @click="onOperator('/')" class="bg-blue-4" label="/"/>
+        </div>
+        <div class="row justify-between q-mx-sm">
+          <CalcBtn @click="wartosc +='7'" label="7"/>
+          <CalcBtn @click="wartosc +='8'" label="8"/>
+          <CalcBtn @click="wartosc +='9'" label="9"/>
+          <CalcBtn @click="onOperator('*')" icon="close" class="bg-blue-4"/>
+        </div>
+        <div class="row justify-between q-mx-sm">
+          <CalcBtn @click="wartosc +='4'" label="4"/>
+          <CalcBtn @click="wartosc +='5'" label="5"/>
+          <CalcBtn @click="wartosc +='6'" label="6"/>
+          <CalcBtn @click="onOperator('-')" icon="remove" class="bg-blue-4"/>
+        </div>
+        <div class="row justify-between q-mx-sm">
+          <CalcBtn @click="wartosc +='1'" label="1"/>
+          <CalcBtn @click="wartosc +='2'" label="2"/>
+          <CalcBtn @click="wartosc +='3'" label="3"/>
+          <CalcBtn @click="onOperator('+')" icon="add" class="bg-blue-4"/>
+        </div>
+        <div class="row justify-between q-mx-sm">
+          <CalcBtn @click="wartosc.endsWith(',') || (wartosc +=',')" label=","/>   <!-- || oznacza ze jezeli to po lewej jest prawda nie wykona tego po prawej i na odwrot -->
+          <CalcBtn @click="wartosc +='0'" label="0"/>
+          <CalcBtn @click="wartosc = wartosc.slice(0,-1)" icon="backspace" class="bg-blue-4"></CalcBtn>
+          <CalcBtn @click="oblicz" icon="drag_handle" class="bg-blue-4"/>
+        </div>
+      </div>
+
+            <!-- <div class="q-pa-md q-gutter-sm bg-blue" v-if="visible"> -->
+          <q-dialog :modelValue="modelValue" @update:modelValue="v => $emit('update:modelValue', v)" >
+            <q-card style="width: 95vw; height: 90vh;" >
+                <q-card-section >
+                  <div class="text-h6">History of calculations</div>
+                </q-card-section>
+
+              <q-separator />
+
+                <q-virtual-scroll
+                   style="height: 70vh;"
+                  :items="todos"
+                  separator
+                  v-slot="{ item, index }"
+                 >
+                      <q-item :key="index" dense>
+                             <q-item-section>
+                                 <q-item-label class="text-h6">
+                                   {{ item }}
+                                 </q-item-label>
+                              </q-item-section>
+                       </q-item>
+                    </q-virtual-scroll>
+              <q-separator />
+
+                <q-card-actions align="right">
+                    <q-btn flat label="Accept" color="green" v-close-popup />
+                    <q-btn @click="todos=[] " icon="delete" color="red"/>
+                </q-card-actions>
+            </q-card>
+          </q-dialog>
+          </div>
+      <!-- </div> -->
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
+import CalcBtn from 'src/components/CalcBtn.vue'
+// import CalcDialog from 'src/components/CalcDialog.vue'
+// import { useQuasar } from 'quasar'
 
-export default defineComponent({
-  name: 'IndexPage'
+// const $q = useQuasar()
+const wartosc = ref('')
+
+defineProps({
+  modelValue: Boolean
 })
+defineEmits(['update:modelValue'])
+
+function oblicz () {
+  const current = wartosc.value
+  wartosc.value = String(eval(wartosc.value.replace(',', '.'))).replace('.', ',') // eslint-disable-line no-eval
+
+  todos.value.push(`${current} = ${wartosc.value}`)
+}
+function onOperator (op) {
+  if (wartosc.value.endsWith(op)) return
+
+  if (wartosc.value.match(/\D$/)) wartosc.value = wartosc.value.slice(0, -1) + op
+  else wartosc.value += op
+}
+const todos = ref([])
+
 </script>
