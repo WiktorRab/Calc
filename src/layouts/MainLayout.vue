940c0cc3970fa873c1637d5fecc2118b5925ba38
WiktorRab <wiktor.rabczuk@wp.pl>
2023-04-01 20:51:25 +0200

    v1.1.4

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,7 +7,7 @@
           Calc App
         </q-toolbar-title>
         <CalcBtn @click="visible = !visible" style="height: 40px;" icon="history" flat />
-        <div>v1.1.3</div>
+        <div>v1.1.4</div>
       </q-toolbar>
     </q-header>
 
